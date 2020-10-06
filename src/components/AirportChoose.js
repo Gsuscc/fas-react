a52b49dcde3983816ca6a8d1acc42c31eedaeecc
attila-h <hauth.attila@gmail.com>
2020-10-06 21:19:32 +0200

    add search delay for input onchange

--- src/components/AirportChoose.js
+++ src/components/AirportChoose.js
@@ -1,35 +1,50 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import flightFetch from "../dataHandler/dataHandler";
 
-export default function AirportChoose(props) {
+const AirportChoose = React.memo((props) => {
   const [airports, setAirports] = useState([]);
   const [text, setText] = useState("");
+  const inputRef = useRef();
 
-  const callBack = useCallback((data) => {
+  const fillOptions = useCallback((data) => {
     setAirports(data.values);
   }, []);
 
   useEffect(() => {
-    if (text.length > 2) {
-      flightFetch(
-        `http://localhost:8080/airport/query?substring=${text}`,
-        callBack
-      );
-    }
-  }, [text, callBack]);
+    const timer = setTimeout(() => {
+      if (text === inputRef.current.value) {
+        if (inputRef.current.value.length >= 2) {
+          const queryUrl = `http://localhost:8080/airport/query?substring=${text}`;
+          flightFetch(queryUrl, fillOptions);
+        } else {
+          setAirports([]);
+        }
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [text, fillOptions]);
 
   return (
     <React.Fragment>
       <input
+        ref={inputRef}
         list="Airport"
         onChange={(event) => setText(event.target.value)}
         value={text}
       />
       <datalist id="Airport">
         {airports.map((airport) => {
-          return <option value={airport.code}>{airport.label}</option>;
+          return (
+            <option value={airport.code} key={airport.code}>
+              {airport.label}
+            </option>
+          );
         })}
       </datalist>
     </React.Fragment>
   );
-}
+});
+
+export default AirportChoose;
