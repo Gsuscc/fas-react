9dd4308cb97bcdd2405463395f2bda27b3aec5aa
Gergely Molnar <molnar.gergo18@gmail.com>
2020-10-07 07:22:39 +0200

    add page title

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,11 @@ export default function Header() {
           <img src="/plane.png" alt="plane" className="plane"></img>
         </a>
       </li>
+      <li className="page-title">
+        <a className="page-title-text" href="/">
+          Fake Airline Services
+        </a>
+      </li>
       <li className="right">
         <a href="#about">About</a>
       </li>
