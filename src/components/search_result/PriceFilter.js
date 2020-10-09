a80be6f8ae565113b4810966ef959c6544db8d95
Gergely Molnar <molnar.gergo18@gmail.com>
2020-10-09 07:24:01 +0200

    try to make this work

--- src/components/search_result/PriceFilter.js
+++ src/components/search_result/PriceFilter.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
+import { GeneralState } from "../../context/SearchGeneral";
 
 const useStyles = makeStyles({
   root: {
@@ -15,10 +16,38 @@ function valuetext(value) {
 
 export default function RangeSlider() {
   const classes = useStyles();
-  const [value, setValue] = React.useState([20, 500]);
+  const { result } = useContext(GeneralState);
+  const [searchResult, setSearchResult] = result;
+  let defaultPriceLow = 20;
+  let defaultPriceMax = 500;
+  if (searchResult.length > 0) {
+    defaultPriceLow =
+      searchResult[0].ticket.touristPrice +
+      (searchResult[0].returnTicket
+        ? searchResult[0].returnTicket.touristPrice
+        : 0);
+
+    defaultPriceMax =
+      searchResult[searchResult.length - 1].ticket.touristPrice +
+      (searchResult[searchResult.length - 1].returnTicket
+        ? searchResult[searchResult.length - 1].returnTicket.touristPrice
+        : 0);
+  }
+  const [value, setValue] = React.useState([defaultPriceLow, defaultPriceMax]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    console.log(newValue[0]);
+    console.log(newValue[1]);
+    let originalResult = searchResult;
+    let newResult = searchResult.filter((obj) => {
+      console.log(obj);
+      let price =
+        obj.ticket.touristPrice +
+        (obj.returnTicket ? obj.returnTicket.touristPrice : 0);
+      return price >= newValue[0] && price <= newValue[1];
+    });
+    setSearchResult(newResult);
   };
 
   return (
@@ -27,7 +56,7 @@ export default function RangeSlider() {
         Price range
       </Typography>
       <Slider
-        max={1000}
+        max={defaultPriceMax}
         value={value}
         onChange={handleChange}
         valueLabelDisplay="auto"
