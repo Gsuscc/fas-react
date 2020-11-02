e2c9eb992add3b4c074f42458595e38b9abc5291
Gergely Molnar <molnar.gergo18@gmail.com>
2020-11-02 15:24:06 +0100

    get texfield values

--- src/components/ui/RegistrationModal.js
+++ src/components/ui/RegistrationModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useContext, useState}from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,6 +6,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => (
   {
@@ -27,6 +28,20 @@ const useStyles = makeStyles((theme) => (
 export default function RegistrationModal(props) {
   const classes = useStyles();
   const { onClose, open } = props;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+
+  const handleClick =()=> {
+      axios.post('http://localhost:8080/auth/register',{
+        username: username,
+        email: email,
+        password: password
+      }).then((response)=>{
+        console.log(response)
+      })
+  }
 
   return (
     <div>
@@ -40,18 +55,24 @@ export default function RegistrationModal(props) {
                 id="name"
                 label="Email Address"
                 type="email"
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
             />
             <TextField
                 margin="dense"
                 id="name"
                 label="Username"
                 type="username"
+                onChange={(event) => setUsername(event.target.value)}
+                value={username}
             />
             <TextField
                 margin="dense"
                 id="name"
                 label="Password"
                 type="password"
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
             />  
           </div>
         </DialogContent>
@@ -59,7 +80,7 @@ export default function RegistrationModal(props) {
           <Button onClick={onClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={onClose} color="primary">
+          <Button onClick={handleClick} color="primary">
             Register
           </Button>
         </DialogActions>
