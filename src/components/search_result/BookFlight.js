092db1deb2baa08794e485eb45fc882c3ec4f57c
attila-h <hauth.attila@gmail.com>
2020-11-03 18:15:50 +0100

    add callback to book flight

--- src/components/search_result/BookFlight.js
+++ src/components/search_result/BookFlight.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { UserState } from '../../context/UserContext';
 import { makeStyles } from '@material-ui/core/styles';
 import flightFetch from '../../dataHandler/dataHandler'
+import { ErrorState } from "../../context/ErrorContext";
+
 const useStyles = makeStyles((theme) => ({
   bookFlight: {
     display: 'flex',
@@ -14,8 +16,9 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
     "&:hover":{
-      transform: 'scale(1.2)'
-
+      transform: 'scale(1.2)',
+      backgroundColor: "#aaffaaaa",
+      cursor: 'pointer'
     }
   },
 }));
@@ -24,8 +27,21 @@ export default function BookFlight(props) {
 
   const { isLoggedIn, setOpenLogin, setLoginDetails } = React.useContext(UserState);
   const classes = useStyles();
+  const toFlight = props.toFlightId;
+  const returnFlight = props.returnFlightId;
+  const { setError } = React.useContext(ErrorState);
+  const [isBooked, setIsBooked] = React.useState(false)
+
   const handleBooking = () => {
-      flightFetch(`http://localhost:8080/favourite/book?id=${props.toFlightId}&returnId=${props.returnFlightId}`, () => null, () => null)
+      let queryparam = `id=${toFlight}`;
+      if (returnFlight) queryparam += "&returnId=" + returnFlight;
+      flightFetch(`http://localhost:8080/favourite/book?${queryparam}`, confirmBooking, (error) => {setError(error)})
+  }
+
+  const confirmBooking = (data) => {
+    console.log("itt")
+    setIsBooked(true)
+    setTimeout(() => setIsBooked(false), 3000)
   }
 
   const handleLogin = () => {
@@ -37,8 +53,9 @@ export default function BookFlight(props) {
   }
 
   return (
-    <div className={classes.bookFlight}>
-      Book this ticket: <img src={isLoggedIn ? "/booking.png" : "/booking-black.png"} onClick={isLoggedIn ? handleBooking : handleLogin } alt="book flight" className="book-icon"></img>
+    <div className={classes.bookFlight} onClick={!isBooked && (isLoggedIn ? handleBooking : handleLogin )}>
+      <img src={isLoggedIn ? "/booking.png" : "/booking-black.png"} alt="book flight" className="book-icon"></img>
+      {isBooked ? "Successfully booked" : "Book this ticket"}    
     </div>
   )
 }
