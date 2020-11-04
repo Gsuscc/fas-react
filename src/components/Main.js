e68a9d7d1bfa6defa91ae9297740708538e89d64
Gergely Molnar <molnar.gergo18@gmail.com>
2020-11-04 08:50:27 +0100

    add info banner about covid-19

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Form from "./form/Form";
 import AdvertiseSwipe from "./specialOffer/AdvertiseSwipe";
+import "../App.css";
+import ImportantInfoBanner from "./ImportantInfoBanner";
 
 export default function Main() {
   return (
     <div>
+      <ImportantInfoBanner />
       <Form />
+      <div className="compare-context">Compare flights!</div>
+      <div className="sub-compare-context">Flight comparison from 20+ cities, 10+ airlines and over 100.000 flights</div>
       <AdvertiseSwipe />
     </div>
   );
